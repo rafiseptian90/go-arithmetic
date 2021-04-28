a1a2f1e07ca697cc93589c7e121fe5b9153eedba
Rafi Septian Hadi <rafiseptian90@gmail.com>
2021-04-28 12:24:01 +0700

    v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rafiseptian90/go-arithmetic
+
+go 1.16
+
+require github.com/rafiseptian90/arithmetic-go-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	arithmetic "github.com/rafiseptian90/arithmetic-go-module"
+)
+
+func main() {
+	result := arithmetic.Sum(120, 210)
+	fmt.Println(result)
+}
